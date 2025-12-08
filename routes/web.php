67138ef51791fd69e023551cbddc75f269380488
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-12-08 23:21:45 +0900

    laravelの基本1

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello/{id?}/{pass?}', [HelloController::class, 'index']);
